f524bf9127957a7a50f35cb28518056864aa5d71
Jose V. Die <jodiera@upv.es>
2018-05-30 17:47:59 -0400

    Update CDname.R

--- R/CDname.R
+++ R/CDname.R
@@ -7,27 +7,29 @@ library(rentrez)
 
 
 ## Define function
-get_CDDacc <- function(prot_acc) {
+get_CDD = function(xp) {
   
   # NCBI Eutils through rentrez pckg.
-    protein <-  entrez_summary(db="protein", id= prot_acc)
-    p_links = entrez_link(dbfrom='protein', id=protein$uid, db='cdd')
-    cdd = p_links$links$protein_cdd_concise_2
-    
+  protein <-  entrez_summary(db="protein", id= xp)
+  p_links = entrez_link(dbfrom='protein', id=protein$uid, db='cdd')
+  cdd = p_links$links$protein_cdd_concise_2
+  
+  
   # initializes an empty vector
-  acc = c()   
+  acc = c()
   
   # if CDD
-    if(! is.null(cdd)) {
-      region = entrez_summary(db="cdd", id= cdd)
-      acc[i] = region$accession
-    
-    } else {acc = 0}
-      
-  acc  
+  if(! is.null(cdd)) {
+    for(i in seq_along(cdd)) {
+      region = entrez_summary(db="cdd", id= cdd[i])
+      acc = c(acc, region$accession)
+    }
 
+  } else {acc = 0}
+  
+  acc
+  
 }
-
 
 ## Usage
 # xps = c("YP_001949746.1", "YP_001949758", "YP_001949763", "YP_001949766", "YP_001949773", "YP_001949775", "YP_001949776", "YP_001949796", "YP_001949797", "YP_001949798", "YP_004306652", "YP_004306653", "YP_004306654", "YP_004306656", "YP_004306662", "YP_004306669", "YP_004306672", "YP_004306675", "YP_004306679", "YP_004306680", "YP_004306696")
